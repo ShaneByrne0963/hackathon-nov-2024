d6b368fccb740c67c2a685aceca78ce4393ff1ed
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-11-14 14:25:33 +0100

    update ruler func to fit with element query

--- ruler.js
+++ ruler.js
@@ -1,14 +1,16 @@
 
 /*This script is used to change the cursor to a wait cursor when the ruler tool is selected. 
 Later on we will change the wait symbol to a ruler.*/
-export default function ruler(rulerClicked) {
+function ruler(element) {
     if (rulerClicked){
-        window.addEventListener('mouseover', function() {
+        element.addEventListener('mouseover', function() {
             document.body.style.cursor = "wait";
         });
     } else {
-        window.addEventListener('mouseover', function() {
+        element.addEventListener('mouseover', function() {
             document.body.style.cursor = "default";
         });
     }
 }
+
+
