2d184dc4847625d7c86a26541652cc43f91965aa
Vasilika Schnitzer <vasilika.schnitzer@gmail.com>
2024-11-15 00:36:34 +0100

    Uncomment line

--- ruler.js
+++ ruler.js
@@ -2,7 +2,7 @@
 /*This script is used to change the cursor to a wait cursor when the ruler tool is selected. 
 Later on we will change the wait symbol to a ruler.*/
 function ruler(element, data) {
-      //  element.style.cursor = "wait";
+        element.style.cursor = "wait";
 }
 
 
