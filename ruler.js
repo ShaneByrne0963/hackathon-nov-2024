43a93a42bbf1618559f7a450ea498b65f333e8b4
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-11-14 11:54:52 +0100

    create ruler function, not tested

---
+++ ruler.js
@@ -0,0 +1,14 @@
+
+/*This script is used to change the cursor to a wait cursor when the ruler tool is selected. 
+Later on we will change the wait symbol to a ruler.*/
+export default function ruler(rulerClicked) {
+    if (rulerClicked){
+        window.addEventListener('mouseover', function() {
+            document.body.style.cursor = "wait";
+        });
+    } else {
+        window.addEventListener('mouseover', function() {
+            document.body.style.cursor = "default";
+        });
+    }
+}
