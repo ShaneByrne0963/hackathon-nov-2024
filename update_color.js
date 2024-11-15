8efe1034dd010aeb4505555ed12895bfdf0e3594
unknown <shanebyrne0963@gmail.com>
2024-11-15 10:55:27 +0000

    Add functions to convert RGB to HSV and vice versa

--- update_color.js
+++ update_color.js
@@ -78,6 +78,74 @@ function getRelativeLuminance(lum1, lum2) {
 }
 
 
+/**
+ * Converts an RGB color value to HSV
+ * Source: https://stackoverflow.com/questions/8022885/rgb-to-hsv-color-in-javascript
+ * @param {Number} r The red value of the color (0-255)
+ * @param {Number} g The green value of the color (0-255)
+ * @param {Number} b The blue value of the color (0-255)
+ * @returns {Object} { h: hue, s: saturation, v: value }
+ */
+function rgb2hsv (r, g, b) {
+  let rabs, gabs, babs, rr, gg, bb, h, s, v, diff, diffc, percentRoundFn;
+  rabs = r / 255;
+  gabs = g / 255;
+  babs = b / 255;
+  v = Math.max(rabs, gabs, babs),
+  diff = v - Math.min(rabs, gabs, babs);
+  diffc = c => (v - c) / 6 / diff + 1 / 2;
+  percentRoundFn = num => Math.round(num * 100) / 100;
+  if (diff == 0) {
+      h = s = 0;
+  } else {
+      s = diff / v;
+      rr = diffc(rabs);
+      gg = diffc(gabs);
+      bb = diffc(babs);
+
+      if (rabs === v) {
+          h = bb - gg;
+      } else if (gabs === v) {
+          h = (1 / 3) + rr - bb;
+      } else if (babs === v) {
+          h = (2 / 3) + gg - rr;
+      }
+      if (h < 0) {
+          h += 1;
+      }else if (h > 1) {
+          h -= 1;
+      }
+  }
+  return {
+      h: Math.round(h * 360),
+      s: percentRoundFn(s * 100),
+      v: percentRoundFn(v * 100)
+  };
+}
+
+
+/**
+ * Converts a HSV color to RGB
+ * Source: https://stackoverflow.com/questions/17242144/javascript-convert-hsb-hsv-color-to-rgb-accurately
+ * @param {Number} h The hue value of the color [0-360]
+ * @param {Float} s The saturation value of the color [0-100]
+ * @param {Float} v The value of the color [0-100]
+ * @returns 
+ */
+function hsv2rgb(h,s,v) {
+  // Converting saturation and value to be between 0 and 1
+  const saturation = (s < 1) ? s : s / 100.0
+  const value = (v < 1) ? v : v / 100.0
+
+  let f= (n,k=(n+h/60)%6) => value - value*saturation*Math.max( Math.min(k,4-k,1), 0);
+  return {
+    r: Math.round(f(5) * 255),
+    g: Math.round(f(3) * 255),
+    b: Math.round(f(1) * 255)
+  };
+}
+
+
 /***
  * Fixes text colors if contrast is not high enough
  * @param {HTMLElement} element the element to be targeted
@@ -95,8 +163,8 @@ function updateColorContrast(element, data) {
 
     if (relativeLuminance < 3) {
       // Will temporarily leave these in so you guys can test it
-      console.log("Element has bad contrast!");
-      console.log(element);
+      console.log('RGB: ', textColor);
+      console.log('HSV: ', rgb2hsv(...textColor));
     }
   }
 }
