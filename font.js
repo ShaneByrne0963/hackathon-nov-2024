aae2a3731b1670d737985e714758e9e70171422f
llancruzz <alancruzsilva@gmail.com>
2024-11-16 17:37:25 +0000

    feat: Create functionality to change size and font elements

--- font.js
+++ font.js
@@ -1,9 +1,28 @@
 function setFontFamily(element, data) {
   const fontFamily = data.fontFamily;
+  // Add style to body
+  const styleGlobal = document.createElement("style");
+  styleGlobal.innerHTML = `
+         * {
+             font-family: ${fontFamily} !important;
+             line-height: 1.5 !important;
+         }
+     `;
+  document.head.appendChild(styleGlobal);
   console.log(fontFamily);
 }
 
 function setFontSize(element, data) {
   const fontSize = data.fontSize;
+  // Add style to body
+  const styleGlobal = document.createElement("style");
+  styleGlobal.innerHTML = `
+         * {
+
+             font-size: ${fontSize}px !important;
+             line-height: 1.5 !important;
+         }
+     `;
+  document.head.appendChild(styleGlobal);
   console.log(fontSize);
 }
