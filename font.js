e0aafe12347a857e990ce7b5d8f2e91f2a8d9695
llancruzz <alancruzsilva@gmail.com>
2024-11-17 09:35:20 +0000

    fix: Multiple font styles css created in DOM

--- font.js
+++ font.js
@@ -1,15 +1,26 @@
 function setFontFamily(element, data) {
   const fontFamily = data.fontFamily;
-  // Add style to body
-  const styleGlobal = document.createElement("style");
-  styleGlobal.innerHTML = `
-         * {
-             font-family: ${fontFamily} !important;
-             line-height: 1.5 !important;
-         }
-     `;
-  document.head.appendChild(styleGlobal);
-  console.log(fontFamily);
+
+  let existingStyle = document.querySelector(".accessorease-font-style");
+
+  if (existingStyle) {
+    existingStyle.remove();
+  }
+
+  if (fontFamily !== "Default") {
+    // Add style to body
+    const styleGlobal = document.createElement("style");
+    styleGlobal.innerHTML = `
+           * {
+               font-family: ${fontFamily} !important;
+               line-height: 1.5 !important;
+           }
+       `;
+
+    styleGlobal.className = "accessorease-font-style";
+
+    document.head.appendChild(styleGlobal);
+  }
 }
 
 function setFontSize(element, data) {
