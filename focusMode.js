aded659cc40f23eaf89ad04f84249d08bc538ded
llancruzz <alancruzsilva@gmail.com>
2024-11-17 14:33:24 +0000

    fix: Update index.html page, manifest.json

--- focusMode.js
+++ focusMode.js
@@ -1,22 +1,4 @@
-// Variable to track focus mode state
-let isFocusModeOn = false;
-
 function setFocusMode(data) {
-  const focusModeButton = document.getElementById("focusModeButton");
-
   const focusMode = data.focusMode;
-
-  isFocusModeOn = !isFocusModeOn;
-
-  if (isFocusModeOn) {
-    focusModeButton.textContent = "Deactivate Focus Mode";
-    console.log("Focus Mode is now ON");
-  } else {
-    focusModeButton.textContent = "Activate Focus Mode";
-    console.log("Focus Mode is now OFF");
-  }
+  console.log(focusMode);
 }
-
-// focusModeButton.addEventListener("click", setFocusMode);
-
-document.addEventListener("DOMContentLoaded", setFocusMode);
