82e134c310155f2b4e44efc4c6a547eaa3855e0d
Vasilika Schnitzer <vasilika.schnitzer@gmail.com>
2024-11-17 22:16:42 +0100

    Remove all event listeners when clicked

--- disable-autoplay.js
+++ disable-autoplay.js
@@ -1,31 +1,63 @@
 function disableAutoplay(element, data) {
-  // console.log('disable-autoplay running ... ');
+
+  if (element.tagName === 'VIDEO' && !element.getAttribute('accessorease-video-eventlistener')) {
+    console.log('Trying to add event listener to ', element.tagName);
+    const parentSiblings = Array.from(element.parentNode.parentNode.children);
+    const siblings = Array.from(element.parentNode.children);
+    const boundClickHandler = clickHandler.bind(null, data, element);
+    let listenerGroup = [];
 
 
-  if (element.tagName === 'VIDEO' && !data.eventListeners.includes(element)) { // && !element.getAttribute('accessorease-video-eventlistener')) {
-    console.log('trying to add event listnener');
-    // Only set once!!
-    const clickHandler = function (event) {
-      const siblings = Array.from(element.parentNode.parentNode.children);
+    function clickHandler(data, element, event) {
       console.log('Video clicked:', event.target);
-      element.setAttribute('accessorease-ignore', true);
-      element.removeEventListener('click', clickHandler);
-      siblings.forEach(sibling => sibling.removeEventListener('click', clickHandler));
-      element.parentElement.removeEventListener('click', clickHandler);
+      console.log(data.eventListeners.length);
+      let groupIndex = NaN;
+      const group = data.eventListeners;
 
-      console.log('event listener removed:', element.attributes);
+      for (let i = 0; i < data.eventListeners.length; i++) {
+        console.log('looking for matches!!', i, group);
+
+        if (group[i].includes(event.target)) {
+          console.log('Clicked!!', i);
+          groupIndex = i;
+          break;
+        }
+      }
+      if (!isNaN(groupIndex)) {
+        console.log(group[groupIndex]);
+        for (let el of group[groupIndex]) {
+          el.removeEventListener('click', boundClickHandler);
+          console.log('event listener removed:', el);
+        }
+        element.setAttribute('accessorease-ignore', true);
+        data.eventListeners.splice(groupIndex, 1);
+        console.log("Current data: ", data);
+        console.log("Current data: ", data.eventListeners);
+
+      } else {
+        console.log('No matching group found for the clicked target');
+      }
 
     };
 
-    const siblings = Array.from(element.parentNode.parentNode.children);
+    listenerGroup.push(element);
+    listenerGroup.push(element.parentNode);
+    element.addEventListener('click', boundClickHandler);
+    element.parentNode.addEventListener('click', boundClickHandler);
 
-    element.addEventListener('click', clickHandler);
-    data.eventListeners.push([]);
-    element.parentElement.addEventListener('click', clickHandler);
-    siblings.forEach(sibling => sibling.addEventListener('click', clickHandler));
+    siblings.forEach(sibling => {
+      sibling.addEventListener('click', boundClickHandler);
+      listenerGroup.push(sibling);
+    });
+
+    parentSiblings.forEach(sibling => {
+      sibling.addEventListener('click', boundClickHandler);
+      listenerGroup.push(sibling);
+    });
+
     element.setAttribute('accessorease-video-eventlistener', true);
-    console.log('event listener set:', element.attributes);
-
+    console.log('Event listener set for group:', element.tagName);
+    data.eventListeners.push(listenerGroup);
   }
 
   if (!element.getAttribute('accessorease-ignore')) {
@@ -33,11 +65,8 @@ function disableAutoplay(element, data) {
     element.removeAttribute("autoplay");
 
     if (typeof element.pause === "function" && !element.paused) {
-      console.log('Trying to pause');
-
       element.pause();
     }
-
   }
 
   if (element.tagName === 'IFRAME') {
@@ -48,9 +77,6 @@ function disableAutoplay(element, data) {
     } catch (error) {
       console.error("Cannot access iframe content:", error);
     }
-
     return;
   }
-
-
 }
